40b84e229247603a5c7826db0599f6878dfd8a82
prashantsingh9079 <prashant.singh9079@gmail.com>
2023-06-19 18:02:59 +0530

    header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import mealsImage from '../../assests/meals.jpg'
+import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton'
+
+export default function Header() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Laziz Food</h1>
+        <HeaderCartButton> Cart</HeaderCartButton>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="meals" />
+      </div>
+    </>
+  )
+}
