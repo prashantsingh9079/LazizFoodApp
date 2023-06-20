5667d001a5f8a246cb34dfcbbfe775fc62abd741
prashantsingh9079 <prashant.singh9079@gmail.com>
2023-06-20 11:27:03 +0530

    modal added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Layout/Header";
 import Meal from "./components/Meal/Meal";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <>
+    <Cart/>
       <Header/>
       <main>
         <Meal/>
--- src/components/Meal/MealItem/MealItemForm.js
+++ src/components/Meal/MealItem/MealItemForm.js
@@ -2,10 +2,19 @@ import React from 'react'
 import classes from './MealItemForm.module.css'
 import Input from '../../UI/Input'
 
-export default function MealItemForm() {
+export default function MealItemForm(props) {
     return (
         <form className={classes.form}>
-            <Input label='Amount' input={{id:"amount", type:"number", min:'1', max:'1', step:'1', defaultValue:'1'}} />
+            <Input label='Amount' input={
+                {
+                    id: "amount_"+ props.id,
+                    type: "number",
+                    min: '1',
+                    max: '1',
+                    step: '1',
+                    defaultValue: '1'
+                }
+            } />
             <button className={classes.button}> +Add </button>
         </form>
     )
